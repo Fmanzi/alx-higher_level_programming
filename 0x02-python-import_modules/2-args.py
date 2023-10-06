71b00ddb2636b6b9a9c2d567275a352813e7d64c
Fmanzi <fmanzi350@gmail.com>
2023-10-05 17:10:12 -0700

    task 1 and 2 modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_arguments = len(sys.argv) - 1
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arguments))
+
+    for i in range(1, num_arguments + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+    if num_arguments == 0:
+        print(".")
+    else:
+        print()
