7c9521623c61120c1881709b735500dda3b984d0
Fmanzi <fmanzi350@gmail.com>
2023-10-09 18:54:22 -0700

    reversed keyword on lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for element in range(len(my_list), 0, -1):
+    for element in reversed(my_list):
         print("{:d}".format(element))
