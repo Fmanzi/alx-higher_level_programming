6678f3d8e9f62815b988f87dca5273d76ed6428a
Fmanzi <fmanzi350@gmail.com>
2023-10-24 20:00:43 -0700

    corrected

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
 class Square:
+     """Defines a square with a size attribute.
+
+    Args:
+        size (int): The size of the square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
     def __init__(self, size):
         self.__size = size
 
     @property
     def size(self):
+        """int: Retrieve the size of the square."""
         return self.__size
